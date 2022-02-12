3d4607559a82897f225914cc0dd4797ecec18978
Erik Dever <erikdever@hotmail.com>
2022-02-12 16:30:18 -0700

    refactored JS to use single-purpose functions

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const APIkey = 'zoXG3TZxZsDfbNY56jatMqyS1Ut75gGO'
 
-$('form').on('submit', function(evt) {
+$('form').on('submit', async function(evt) {
     evt.preventDefault();
-    getGif($('#search').val());
+    let gif = await getGif($('#search').val());
+    appendDom(gif)
     $('#search').val('')
 })
 
@@ -11,11 +12,15 @@ async function getGif(term){
     const res = await axios.get('https://api.giphy.com/v1/gifs/search', {params:{q: term, api_key: APIkey}})
     const rNum = Math.floor(Math.random() * 50) + 1
     const gifUrl = res.data.data[rNum].images.fixed_height.url;
-    const $img = $('<img>').attr('src', gifUrl).attr('class', 'mx-1 my-1 img-fluid img-thumbnail')
-    $('section').append($img)
+    return gifUrl
     } catch(err) {
         alert('Please enter a real search term!');
     }
+}
+
+function appendDom(gifUrl) {
+    const $img = $('<img>').attr('src', gifUrl).attr('class', 'mx-1 my-1 img-fluid img-thumbnail')
+    $('section').append($img)
 }
 
 $('#removeBtn').on('click', function() {
